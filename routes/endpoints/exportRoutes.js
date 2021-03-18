7643a1197a75ec736e4b2a2d4a138d23ed90473f
jacmurillo1 <sm45040@gmail.com>
2021-03-18 14:48:00 -0500

    fixed the export functionality, so now when endpoint is hit, it auto downloads the database csv

--- routes/endpoints/exportRoutes.js
+++ routes/endpoints/exportRoutes.js
@@ -1,14 +1,10 @@
 const mongoose = require("mongoose");
-const fs = require('fs');
-const moment = require('moment');
 const json2csv = require('json2csv').parse;
-const path = require('path')
 const fields = ['tags', 'date', '_id','name', 'owner', 'ownerID', 'contactInfo','status', 'description', 'gitRepo', 'image', '__v'];
 const router = require("express").Router();
 const projectModel = mongoose.model("Project");
 
 module.exports = function() {
-
     router.get('/', function (req, res) {
         projectModel.find({}).exec(function (err, projects) {
             if (err) {
@@ -21,21 +17,9 @@ module.exports = function() {
                 } catch (err) {
                     return res.status(500).json({ err });
                 }
-                const dateTime = moment().format('YYYYMMDDhhmmss');
-                console.log("date-Time:" + dateTime);
-                const filePath = path.join(__dirname, "..", "csv-" + dateTime + ".csv")
-                fs.writeFile(filePath, csv, function (err) {
-                    if (err) {
-                    return res.json(err).status(500);
-                    }
-                    else {
-                    setTimeout(function () {
-                        fs.unlinkSync(filePath); // delete this file after 30 seconds
-                    }, 30000)
-                    return res.json("/exports/csv-" + dateTime + ".csv");
-                    }
-                }); 
-    
+                res.setHeader('Content-disposition', 'attachment; filename=research_projects.csv');
+                res.set('Content-Type', 'text/csv');
+                res.status(200).send(csv);  
             }
         })
     })
