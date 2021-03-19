877da4f4a87e511aa9f2183f3a4f04edc7ffaa2c
Anthony <ahanel13@gmail.com>
2021-03-19 16:00:51 -0500

    styling navbar with clicl list items

--- client/src/components/AdminPage/Dashboard.jsx
+++ client/src/components/AdminPage/Dashboard.jsx
@@ -6,22 +6,30 @@ import PropTypes from "prop-types";
 import './Dashboard.scss';
 
 class AdminDashboard extends Component {
-
-    render(){
-        return (
-          <div>
-            <div id="Dashboard-container">
-              <div id="Dashboard-Nav">
-                <h2 id="Sidebar-Title">Admin Navigation</h2>
-              </div>
-              <div id="Dashboard-Content">
-                <h1 id="AdminDashboard-h1">Portal Administration</h1>
-
-              </div>
-            </div>
-          </div>
-        );
-    }
+  render(){
+    return (
+      <div id="Dashboard-container">
+        <div id="Dashboard-Nav">
+          <h2 id="Sidebar-Title">Admin Navigation</h2>
+          <ul id="Sidebar-List">
+            <hr/>
+            <li id="Dashboard">Dashboard</li>
+            <hr/>
+            <li id="Pending-Projects">Pending Projects</li>
+            <hr/>
+            <li id="All-Users">All Users</li>
+            <hr/>
+            <li id="Edit-Projects">Edit Projects</li>
+            <hr/>
+            <li id="Tags">Tags</li>
+          </ul>
+        </div>
+        <div id="Dashboard-Content">
+          <h1 id="Content-Title">Portal Administration</h1>
+        </div>
+      </div>
+    );
+  }
 };
 
 AdminDashboard.propTypes = {
