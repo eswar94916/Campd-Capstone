0f1faed2ec459df6e15c63bdaf28815becbbaccd
Anthony <ahanel13@gmail.com>
2021-03-23 16:54:14 -0500

    completed adding the list of all projects to the edit projects section. I will need to link the search bar and convert the list to a html select for editing

--- client/src/components/AdminPage/EditProjects.jsx
+++ client/src/components/AdminPage/EditProjects.jsx
@@ -1,16 +1,47 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 import ProjectSearch from '../ProjectsPage/ProjectSearch';
+import { fetchAllProjects } from '../../actions';
+import ProjectList from '../ProjectsPage/ProjectListContainer.jsx';
+import Project from './EditProjectListing';
 
 class EditProjects extends Component {
+    componentDidMount() {
+        this.props.onMount()
+    }
+
+
 
     render(){
         return (
             <div id="Dashboard-Content">
                 <h1 id="Content-Title">Edit Projects</h1>
                 <ProjectSearch />
+                    {this.props.projects.map((project, index) => {
+                        return (
+                            <Project project={ project } key={ project._id } index={index}/>
+                        )
+                    })}
             </div>
         )
     }
 }
 
-export default (EditProjects);
+const mapStateToProps = state => {
+  return {
+    projects: state.projects
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onMount: id => {
+      dispatch(fetchAllProjects())
+    }
+  };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(EditProjects);
