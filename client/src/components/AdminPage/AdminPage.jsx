1b0d39d97b6aaa98b26db3ca891b420772ae5a8f
Anthony <ahanel13@gmail.com>
2021-03-23 12:52:42 -0500

    trying to get the edit project component to render

--- client/src/components/AdminPage/AdminPage.jsx
+++ client/src/components/AdminPage/AdminPage.jsx
@@ -2,34 +2,52 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { fetchAllProjects } from '../../actions';
+import EditProjects from "./EditProjects";
+import ProjectSearch from '../ProjectsPage/ProjectSearch';
+
 
 
 import './AdminPage.scss';
 
 class AdminDashboard extends Component {
   
+  constructor(props) {
+    super(props);
+    this.state = {
+      // Dashboard, AllUsers, EditProjects, Tags, Import, Export
+      displaydisplayContainer: 0
+    }
+  }
+
   handleDashboard = (event) => {
+    this.state.displayContainer = 0;
     document.getElementById("Content-Title").innerHTML = "Admin Dashboard";
   };
   
   //this will update the dashboard container's contai with unapproved projects
   handlePendingProjects = (event) => {
+    this.state.componentDisplay = [0, 1, 0, 0, 0, 0 ];
     document.getElementById("Content-Title").innerHTML = "Pending Projects";
   };
   
   handleAllUsers = (event) => {
+    this.state.componentDisplay = [0, 0, 1, 0, 0, 0 ];
     document.getElementById("Content-Title").innerHTML = "All Users";
   };
   
   handleEditProjects = (event) => {
+    this.state.displayContainer = 3;
+    console.log(`displayContainer: ${this.state.displayContainer}`);
     document.getElementById("Content-Title").innerHTML = "Edit Projects";
   };
   
   handlTags = (event) => {
+    this.state.componentDisplay = [0, 0, 0, 0, 1, 0 ];
     document.getElementById("Content-Title").innerHTML = "Tags";
   };
 
   handlImport = (event) => {
+    this.state.componentDisplay = [0, 0, 0, 0, 0, 1 ];
     document.getElementById("Content-Title").innerHTML = "Import Projects";
   };
 
@@ -38,6 +56,7 @@ class AdminDashboard extends Component {
   }
   
   render(){
+    const displayContainer = this.state.displayContainer;
     return (
       <div id="Dashboard-Container">
         <div id="Dashboard-Nav">
@@ -61,6 +80,7 @@ class AdminDashboard extends Component {
         </div>
         <div id="Dashboard-Content">
           <h1 id="Content-Title">Admin Dashboard</h1>
+          {displayContainer == 3 && <EditProjects/>}
         </div>
       </div>
     );
---
+++ client/src/components/AdminPage/EditProjects.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import ProjectSearch from '../ProjectsPage/ProjectSearch';
+
+class EditProjects extends Component {
+
+    render(){
+        return (
+            <div id="Edit-Projects-Container">
+                <ProjectSearch />
+            </div>
+        )
+    }
+}
+
+export default (EditProjects);
