ef9b1e20495525dc0b9702fa49856e3175ca0e3e
Hanna <pancakebatter12345@gmail.com>
2021-03-25 15:32:00 -0500

    clean up export

--- routes/endpoints/exportRoutes.js
+++ routes/endpoints/exportRoutes.js
@@ -1,28 +1,104 @@
 const mongoose = require("mongoose");
-const json2csv = require('json2csv').parse;
-const fields = ['tags', 'date', '_id','name', 'owner', 'ownerID', 'contactInfo','status', 'description', 'gitRepo', 'image', '__v'];
+const { Parser } = require("json2csv");
+//const fields = ['tags', 'date', '_id','name', 'owner', 'ownerID', 'contactInfo','status', 'description', 'gitRepo', 'image', '__v'];
 const router = require("express").Router();
 const projectModel = mongoose.model("Project");
+const userModel = mongoose.model("User");
 
-module.exports = function() {
-    router.get('/', function (req, res) {
-        projectModel.find({}).exec(function (err, projects) {
-            if (err) {
-                return res.status(500).json({ err });
-            }
-            else {  
-                let csv
-                try {
-                    csv = json2csv(projects, { fields });
-                } catch (err) {
-                    return res.status(500).json({ err });
-                }
-                res.setHeader('Content-disposition', 'attachment; filename=research_projects.csv');
-                res.set('Content-Type', 'text/csv');
-                res.status(200).send(csv);  
-            }
-        })
-    })
+const fields = [
+    {
+        label: "Project Title",
+        value: "name",
+    },
+    {
+        label: "Owner",
+        value: "owner",
+    },
+    {
+        label: "Owner EUID",
+        value: "ownerID",
+    },
+    {
+        label: "Contact Info",
+        value: "contactInfo",
+    },
+    {
+        label: "Status: Approved",
+        value: "statuses.isApproved",
+    },
+    {
+        label: "Status: New",
+        value: "statuses.isNew",
+    },
+    {
+        label: "Status: Proposal",
+        value: "statuses.isProposal",
+    },
+    {
+        label: "Status: Active",
+        value: "statuses.isActive",
+    },
+    {
+        label: "Status: Recruiting",
+        value: "statuses.isRecruiting",
+    },
+    {
+        label: "Status: Paused",
+        value: "statuses.isPaused",
+    },
+    {
+        label: "Status: Stopped",
+        value: "statuses.isStopped",
+    },
+    {
+        label: "Status: Archived",
+        value: "statuses.isArchived",
+    },
+    {
+        label: "Description",
+        value: "description",
+    },
+    {
+        label: "Repo Link",
+        value: "gitRepo",
+    },
+    {
+        label: "Tags",
+        value: "tags",
+    },
+    {
+        label: "Date Created",
+        value: "date",
+    },
+];
+
+const options = { fields };
+const parser = new Parser(options);
+
+module.exports = function () {
+    router.get("/", async function (req, res) {
+        var jsonData = [];
+        var allProjects = await projectModel.find({});
+        for await (const thisProject of allProjects) {
+            var newJsonProject = thisProject.toObject();
+            console.log(newJsonProject);
+            var thisUser = await userModel.findById(thisProject.ownerID);
+            newJsonProject.ownerID = thisUser.euid;
+            newJsonProject.tags = null;
+            newJsonProject.tags = thisProject.tags.join(", ");
+            console.log(newJsonProject.tags);
+            jsonData.push(newJsonProject);
+        }
+
+        try {
+            var csv = parser.parse(jsonData);
+        } catch (err) {
+            return res.status(500).json({ err });
+        }
+        res.setHeader("Content-disposition", "attachment; filename=research_projects.csv");
+        res.set("Content-Type", "text/csv");
+        res.status(200).send(csv);
+    });
 
     return router;
 };
