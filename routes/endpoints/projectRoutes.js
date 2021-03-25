283cb7f753238ce8fe06488629d5431a0fc434ad
Hanna <pancakebatter12345@gmail.com>
2021-03-24 19:01:46 -0500

    remove tags endpoint

--- routes/endpoints/projectRoutes.js
+++ routes/endpoints/projectRoutes.js
@@ -165,7 +165,72 @@ module.exports = function (gfs) {
     });
 
     /* -------------------------------------------------------------------------- */
-    /*                           Adds status to a project                           */
+    /*                      Remove tags from projectModel(s)                      */
+    /* -------------------------------------------------------------------------- */
+    router.post("/removetags", auth.required, async function (req, res) {
+        if (!req.body.hasOwnProperty("projectID")) {
+            res.status(400).json({
+                errors: "Must include project database ID to edit",
+            });
+        } else if (!req.body.hasOwnProperty("tags")) {
+            res.status(200).send("No tags were added");
+        } else {
+            let projectID = req.body.projectID;
+            let removeTags = req.body.tags;
+
+            let thisProject, thisUser;
+
+            /**
+             * Find the project(s) we are looking for
+             */
+            try {
+                thisUser = await userModel.findOne({ _id: req.user.id });
+                if (!thisUser) {
+                    throw "user";
+                }
+                if (Array.isArray(projectID)) {
+                    for await (const thisID of projectID) {
+                        thisProject = await projectModel.findOne({ _id: thisID });
+                        if (!thisProject) {
+                            throw "project";
+                        }
+
+                        thisProject.tags = thisProject.tags.filter(function (el) {
+                            return !removeTags.includes(el);
+                        });
+                        await thisProject.save();
+                    }
+                } else {
+                    thisProject = await projectModel.findOne({ _id: projectID });
+                    if (!thisProject) {
+                        throw "project";
+                    }
+
+                    thisProject.tags = thisProject.tags.filter(function (el) {
+                        return !removeTags.includes(el);
+                    });
+                    await thisProject.save();
+                }
+            } catch (err) {
+                console.log(err);
+                switch (err) {
+                    case "user":
+                        res.status(400).send("No such user");
+                        break;
+                    case "project":
+                        res.status(400).send("Could not find project");
+                        break;
+                    default:
+                        res.status(500).send(err);
+                }
+            }
+
+            res.status(200).send("okay");
+        }
+    });
+
+    /* -------------------------------------------------------------------------- */
+    /*                           Adds status to a project                         */
     /* -------------------------------------------------------------------------- */
 
     router.post("/updateStatus", auth.required, async function (req, res) {
