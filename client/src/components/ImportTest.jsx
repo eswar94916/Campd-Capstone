4cb38a4b3dc76213e10a39927a3535ddb2f5a8bb
sammmbuu <30850423+sammmbuu@users.noreply.github.com>
2021-03-29 14:36:55 -0500

    Added test component for testing file upload

---
+++ client/src/components/ImportTest.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import axios from "axios";
+
+export default class ImportTest extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            file: null
+        }
+    }
+
+    onFileChange = event => {
+    
+        this.setState({ file: event.target.files[0] });
+      
+    };
+
+    handleSubmit = () => {
+        // Create an object of formData
+      const formData = new FormData();
+    
+      // Update the formData object
+      formData.append(
+        "myFile",
+        this.state.selectedFile,
+        this.state.selectedFile.name
+      );
+    
+      // Details of the uploaded file
+      console.log(this.state.selectedFile);
+    
+      // Request made to the backend api
+      // Send formData object
+      axios.post("api/uploadfile", formData);
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="file" onChange={this.onFileChange}/>
+                <Button onClick={this.handleSubmit}>Submit</Button>
+            </div>
+        )
+    }
+}
