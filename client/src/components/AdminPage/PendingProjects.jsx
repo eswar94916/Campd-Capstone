b2264a410c22a8fbf96aa9a32675bcf58998e17f
sammmbuu <30850423+sammmbuu@users.noreply.github.com>
2021-03-30 14:22:59 -0500

    Projects now have modal that pops up when admin wants to view more info

--- client/src/components/AdminPage/PendingProjects.jsx
+++ client/src/components/AdminPage/PendingProjects.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Card, Container, Col, Row, Dropdown, DropdownButton } from "react-bootstrap";
+import { Button, Container, Col, Row, Modal } from "react-bootstrap";
 import { connect } from 'react-redux';
 import { deleteProject, viewProjects } from '../../actions';
 import "./PendingProjects.scss";
@@ -12,9 +12,17 @@ class PendingProjects extends Component {
   constructor(props) {
       super(props);
       this.state = {
-          selectedProject: {},
+          selectedProject: null,
           showModal: false
       }
+  }
+
+  showProject = (project) => {
+    this.setState({selectedProject: project, showModal: true});
+  }
+
+  hideProject = () => {
+    this.setState({selectedProject: null, showModal: false})
   }
 
   approve = (project) => {
@@ -33,6 +41,7 @@ class PendingProjects extends Component {
       console.log(err);
     })
 
+    this.setState({selectedProject: null, showModal: false})
   }
 
   handleDelete = (project) => {
@@ -40,36 +49,72 @@ class PendingProjects extends Component {
       axios.delete(`upload/${project.image}`);
     }
     this.props.onDelete(project._id)
+    this.setState({selectedProject: null, showModal: false})
   }
 
   render(){
     return (
-        <div id="Dashboard-Content">
-          <h1 id="Content-Title">Pending Projects</h1>
-          <div className="pendingProjects">
-            {this.props.projects.filter(project => !project.statuses.isApproved).map((project, index) => {
-                return (
-                    <Container key={index} className="pendingProject-container">
-                      <Row style={{height: "100%"}}>
-                        <Col md={9} className="pendingProjects-nameAndDesc">
-                          <p className="pendingProjects-name">{project.name}</p>
-                          <p className="pendingProjects-description">{project.description}</p>
-                          <p className="pendingProjects-owner">Project Idea Submitted by {project.owner}, contact {project.contactInfo} for more information.</p>
-                          <p className="pendingProjects-owner">View More Info...</p>
-                        </Col>
-                        <Col md={3} className="pendingProjects-buttonContainer">
-                          <Button className="pendingProjects-button" variant="success" onClick={() => this.approve(project)}>Approve</Button>
-                          <Button className="pendingProjects-button" variant="danger" onClick={() => this.handleDelete(project)}>Delete</Button>
-                        </Col>
-                      </Row>
-                    </Container>
-                )
-            })}
-          </div>
+      <div id="Dashboard-Content">
+        {/******************************************************
+           This section contains the modal that pops up when the
+           admin selects "view more info" from the page
+        ******************************************************/}
+        <Modal show={this.state.showModal} onHide={this.hideProject} backdrop="static">
+          <Modal.Header closeButton>
+            <Modal.Title>
+              {this.state.selectedProject && this.state.selectedProject.name}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {this.state.selectedProject && (this.state.selectedProject.image === "" ? <></> : <img className="pendingProjects-modalImage" src={`image/${this.state.selectedProject.image}`} alt="project" />)}
+            <p>
+              <span style={{fontWeight: "bold"}}>Description: </span>{this.state.selectedProject && this.state.selectedProject.description}
+            </p>
+            <p>
+              <span style={{fontWeight: "bold"}}>Tags: </span>{this.state.selectedProject && this.state.selectedProject.tags.join(" ")}
+            </p>
+            <p>
+              <span style={{fontWeight: "bold"}}>GitHub Link: </span>{this.state.selectedProject && (this.state.selectedProject.gitRepo === "" ? "No GitHub Provided" : this.state.selectedProject.gitRepo)}
+            </p>
+            <p>
+              <span style={{fontWeight: "bold"}}>Submitted By: </span>{this.state.selectedProject && this.state.selectedProject.owner}
+            </p>
+            <p>
+              <span style={{fontWeight: "bold"}}>Contact: </span>{this.state.selectedProject && this.state.selectedProject.contactInfo}
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="success" onClick={() => this.approve(this.state.selectedProject)}>Approve</Button>
+            <Button variant="danger" onClick={() => this.handleDelete(this.state.selectedProject)}>Delete</Button>
+            <Button variant="secondary" onClick={() => {this.hideProject()}}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+        <h1 id="Content-Title">Pending Projects</h1>
+        <div className="pendingProjects">
+          {this.props.projects.filter(project => !project.statuses.isApproved).map((project, index) => {
+              return (
+                  <Container key={index} className="pendingProject-container">
+                    <Row style={{height: "100%"}}>
+                      <Col md={9} className="pendingProjects-nameAndDesc">
+                        <p className="pendingProjects-name">{project.name}</p>
+                        <p className="pendingProjects-description">{project.description}</p>
+                        <p className="pendingProjects-owner">Project Idea Submitted by {project.owner}, contact {project.contactInfo} for more information.</p>
+                        <p className="pendingProjects-viewMore pendingProjects-owner" onClick={() => this.showProject(project)}>View More Info...</p>
+                      </Col>
+                      <Col md={3} className="pendingProjects-buttonContainer">
+                        <Button className="pendingProjects-button" variant="success" onClick={() => this.approve(project)}>Approve</Button>
+                        <Button className="pendingProjects-button" variant="danger" onClick={() => this.handleDelete(project)}>Delete</Button>
+                      </Col>
+                    </Row>
+                  </Container>
+              )
+          })}
         </div>
+      </div>
     )
   }
 }
+
 const mapStateToProps = state => {
     return {
       projects: state.projects
