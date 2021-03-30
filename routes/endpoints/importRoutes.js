4a67e54f311306ff5373cde504ba2e9817575242
jasminesyankaur <jasminesyan21@gmail.com>
2021-03-30 15:04:15 -0500

    project is imprting to Db from local file!

--- routes/endpoints/importRoutes.js
+++ routes/endpoints/importRoutes.js
@@ -1,20 +1,41 @@
 const mongoose = require("mongoose");
 const passport = require("passport");
 const csvtojson = require("csvtojson");
+//const fastcsv = require("fast-csv");
 const router = require("express").Router();
 const projectModel = mongoose.model("Project");
+const fs = require("fs");
+var async = require('async');
 
-module.exports = function (gfs) {
-    // Defined store route
-    router.post("/add", function (req, res) {
-        csvtojson()
-        .fromFile(req.body)
+
+csvtojson()
+        .fromFile("angel.csv")
         .then(csvData => {
-            console.log(csvData);
-    });
+            async.eachSeries(csvData,(data,callback) => {
+                  let entity = {
+                    name: data.name,
+                    owner: data.owner,
+                    ownerID: data.ownerID,
+                    contactInfo: data.contactInfo,
+                    status: data.status,
+                    description: data.description,
+                    tags: data.tags,
+                    gitRepo: data.gitRepo,
+                    image: data.image,
+                    userGuide: data.userGuide,
+                    developerGuide: data.developerGuide,
+                    installationGuide: data.installationGuide
+                    };
+                    projectModel.create({entity}, function(err)
+                    {
+                        if(err) return callback(err);
+                        return callback(null);    
+                    })
+               },
+                (err) => {
+                     if(err) console.log(err); 
+                     console.log("projects are successfully imported!!!");
+                     return promise;
+                });
 
-
-
-return router;
 });
-};
