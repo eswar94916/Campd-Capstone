ee71ed2f637d718ea30b6873d032993e35dabdd9
Anthony <ahanel13@gmail.com>
2021-03-31 15:37:50 -0500

    updated admin routing

---
+++ client/src/actions/PrivateAdminRoute.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+const PrivateAdminRoute = ({ component: Component, auth, ...rest }) => (
+  
+  <Route
+    {...rest}
+    render={props =>
+      auth.isAuthenticated === true ? (
+          auth.isAdmin === true ? (
+            <Component {...props} />
+          ) : (
+            <Redirect to="/" />
+          )
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
+PrivateAdminRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+export default connect(mapStateToProps)(PrivateAdminRoute);
