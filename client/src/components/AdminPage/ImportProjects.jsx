233e264bb7e4bd38d24cbbb2b54cedf1c85cc5e4
sammmbuu <30850423+sammmbuu@users.noreply.github.com>
2021-04-06 13:21:19 -0500

    began work on import

--- client/src/components/AdminPage/ImportProjects.jsx
+++ client/src/components/AdminPage/ImportProjects.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 class ImportProjects extends Component {
 
@@ -6,6 +7,15 @@ class ImportProjects extends Component {
         return (
             <div id="Dashboard-Content">
                 <h1 id="Content-Title">Import Projects</h1>
+                <div className="import-template-container">
+                    <h2>Click the link below to download an import template</h2>
+                    <a href="import-template.csv" download="import-template.csv">Download</a>
+                </div>
+                <div className="import-upload-container">
+                    <h2>Upload your import file here</h2>
+                    <input type="file">Select File</input>
+                    <Button onClick={this.handleSubmit}>Submit</Button>
+                </div>
             </div>
         )
     }
