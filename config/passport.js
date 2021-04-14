3150f1506e7adc86bafd44bc07fddbff332810e3
Hanna <pancakebatter12345@gmail.com>
2021-04-14 16:14:56 -0500

    commented passport

--- config/passport.js
+++ config/passport.js
@@ -4,7 +4,6 @@ const User = mongoose.model("User");
 var ldap = require("ldapjs");
 
 module.exports = function (passport) {
-    console.log("configuring");
     // used to serialize the user for the session
     passport.serializeUser(function (user, done) {
         done(null, user._id);
@@ -17,15 +16,23 @@ module.exports = function (passport) {
         });
     });
 
+    /**
+     * Creating a local strategy because none of the
+     * existing LDAP passport strategies do exactly
+     * what we want
+     */
     passport.use(
         new LocalStrategy(
+            //grab the correct incoming fields
             {
                 usernameField: "euid",
                 passwordField: "password",
             },
             (euid, password, done) => {
-                //create the ldap client
-                console.log("working");
+                /**
+                 * Hardcoded admin user, yes I know it's bad
+                 * it was for testing...
+                 */
                 if (euid == "aaa9999" && password == "admin") {
                     var adminUser = {
                         euid: "aaa9999",
@@ -36,6 +43,10 @@ module.exports = function (passport) {
                         isAdmin: true,
                     };
 
+                    /**
+                     * Either find or create the admin user in the local database
+                     * and return that user.
+                     */
                     User.findOne({ euid: adminUser.euid }, function (err, result) {
                         if (err) {
                             console.log(err);
@@ -54,11 +65,19 @@ module.exports = function (passport) {
                             }
                         }
                     });
-                } else {
-                    var login = ldap.createClient({
-                        url: "ldaps://ldap-id.untsystem.edu",
-                    });
 
+                    /**
+                     * Logging in with the dummy admin account never creates the LDAP
+                     * client to save time
+                     */
+                } else {
+                    //Create the LDAP client for logging in
+                    var login = ldap.createClient({ url: "ldaps://ldap-id.untsystem.edu" });
+
+                    /**
+                     * Sometimes LDAP kicks us out on idle too long? This usually never fires
+                     * because the client is disconnected after each attempt
+                     */
                     login.addListener("error", function (err) {
                         console.log("LDAP kicked us out, but it's fine I think", err);
                     });
@@ -69,10 +88,15 @@ module.exports = function (passport) {
                     //bind the ldap client using supplied credentials
                     login.bind(loginDN, password, function (err, res) {
                         if (err) {
+                            //unbind the LDAP client so we don't get idle errors
                             login.unbind();
                             return done(null, false, {
                                 message: "Password not recognised. Try again?",
                             });
+                            /**
+                             * There are some more descriptive error messages,
+                             * but we don't really care most of the time
+                             */
                         } else {
                             /**
                              * Login was sucessful. Now we ask who the person is
@@ -164,76 +188,7 @@ module.exports = function (passport) {
                         }
                     });
                 }
-
-                /*
-            Users.findOne({ euid })
-                .then((user) => {
-                    if (!user || !user.validatePassword(password)) {
-                        return done(null, false, { errors: { "email or password": "is invalid" } });
-                    }
-
-                    return done(null, user);
-                })
-                .catch(done);
-            */
-
-                //connect to ldap and ask if the username and password is valid
-
-                //if so, look in the database to find an existing match
-
-                //if match, return that document
-
-                //if no match, make the document then return it
-
-                //if not valid, return invalid message
-
-                //return done(null, { object: "yo" }, null);
             }
         )
     );
 };
-
-/*
-Example result from searching
-
-for each person returned
-entry: {
-    dn: "uid=hcf0018,ou=people,o=unt",
-    controls: [],
-    preferEmployeeMail: "Hanna.Flores@unt.edu",
-    preferStudentMail: "HannaFlores@my.unt.edu",
-    employeeMail: "Hanna.Flores@unt.edu",
-    studentMail: "HannaFlores@my.unt.edu",
-    eduPersonScopedAffiliation: ["student@unt.edu", "member@unt.edu", "staff@unt.edu"],
-    eduPersonAffiliation: ["student", "member", "staff"],
-    role: ["untst", "untfs"],
-    euid: "hcf0018",
-    prefname: "Hanna",
-    emplid: "11197290",
-    mail: "HannaFlores@my.unt.edu",
-    uid: "hcf0018",
-    givenName: "Hanna",
-    untjobtitle: "Student Assistant - Regular",
-    sn: "Flores",
-    ou: "University Library-Gen",
-    objectClass: [
-        "inetOrgPerson",
-        "untperson",
-        "organizationalPerson",
-        "person",
-        "ndsLoginProperties",
-        "top",
-        "eduPerson",
-        "mailProperties",
-    ],
-    groupMembership: [
-        "cn=AdobeCloud,ou=UNTGroups,o=unt",
-        "cn=DuoUsers,ou=DuoSecurity,o=unt",
-        "cn=AdobeStudents,ou=UNTGroups,o=unt",
-    ],
-    cn: "Hanna Claire Flores",
-}
-
-final status
-status: 0,
-*/
